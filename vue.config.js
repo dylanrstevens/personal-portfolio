cb7b8196829af13bda1775bbb68ce2798fc2bfa2
Dylan Stevens <79028721+dylanrstevens@users.noreply.github.com>
2022-12-27 23:32:44 -0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    publicPath: "/personal-portfolio/"
+    publicPath: module.exports = {
+      publicPath: process.env.NODE_ENV === "production" ? "/personal-portfolio/" : "/",
+    };
 };
